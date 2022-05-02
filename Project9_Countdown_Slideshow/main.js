802dd09ce150860d26aaf5c5a202dc9760f231d1
sunelement <102632921+sunelement@users.noreply.github.com>
2022-05-01 20:34:18 -0700

    9

---
+++ Project9_Countdown_Slideshow/main.js
@@ -0,0 +1,14 @@
+// Countdown function that sets a timer based on inputed seconds value 
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+        }
+    }
+    tick();
+}
