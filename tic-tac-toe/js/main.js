0beca181094181977a29b32c84db5c8eb4d97350
sunelement <102632921+sunelement@users.noreply.github.com>
2022-05-03 14:01:47 -0700

    tic

---
+++ tic-tac-toe/js/main.js
@@ -0,0 +1,147 @@
+// Global variables used to keep track of game state 
+let activePlayer = 'X';
+let selectedSquares = [];
+
+// Function that resets the board after a win or tie
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i));
+        square.style.backgroundImage = '';
+    }
+    selectedSquares = [];
+}
+
+// Function that draws a line across the 3 in a row using canvas and its coords
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    // Gets the canvas element to draw on it 
+    const canvas = document.getElementById('win-lines');
+    const c = canvas.getContext('2d');
+    let x1 = coordX1,
+        y1 = coordY1,
+        x2 = coordX2,
+        y2 = coordY2,
+        x = x1,
+        y = y1;
+
+
+    // Function that interacts with the canvas drawing the line in animated way  
+    function animateLineDrawing() {
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        // Defines the animation pattern 
+        c.clearRect(0, 0, 608, 608);
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        c.stroke();
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) { y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
+
+    // Nested function that clears the board after the winning line is drawn 
+    function clear() {
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+
+    // Final steps after a winner is choosen  
+    disableClick();
+    audio('./media/winGame.mp3');
+    animateLineDrawing();
+    setTimeout(function () { clear(); resetGame(); }, 1000);
+}
+
+
+// Function called whenever a player makes a move to see if their is a winner
+function checkWinConditions() {
+    // If statements for all the possible win cases
+    if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
+    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
+    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
+    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558) }
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
+    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
+    else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100) }
+    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304) }
+    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 508, 558, 508) }
+    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558) }
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558) }
+    else if (arrayIncludes('20', '50', '80')) { drawWinLine(508, 50, 508, 558) }
+    else if (arrayIncludes('60', '40', '20')) { drawWinLine(100, 508, 510, 90) }
+    else if (arrayIncludes('00', '40', '80')) { drawWinLine(100, 100, 520, 520) }
+    // Checks if it is a draw 
+    else if (selectedSquares.length >= 9) {
+        audio('./media/tie.mp3');
+        setTimeout(function () { resetGame(); }, 1000);
+    }
+
+    // Nested function that checks if 3 squares have been selected
+    function arrayIncludes(squareA, squareB, squareC) {
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+        // Checks if all 3 are selected, win if so 
+        if (a === true && b === true && c === true) { return true; }
+    }
+}
+
+// Makes the screen unclickable for the player while the computer makes move 
+function disableClick() {
+    body.style.pointerEvents = 'none';
+    setTimeout(function () { body.style.pointerEvents = 'auto'; }, 1000);
+}
+
+// Function that plays audio sounds
+function audio(audioURL) {
+    let audio = new Audio(audioURL);
+    audio.play();
+}
+
+// Function called whenever ether player picks a square on their turn
+function placeXOrO(squareNumber) {
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        let select = document.getElementById(squareNumber);
+        // Outcome differs depending on the symbol the player is using 
+        if (activePlayer === 'X') {
+            select.style.backgroundImage = 'url("images/x.png")';
+        } else {
+            select.style.backgroundImage = 'url("images/o.png")';
+        }
+        selectedSquares.push(squareNumber + activePlayer);
+        checkWinConditions();
+        if (activePlayer === 'X') {
+            activePlayer = 'O';
+        } else {
+            activePlayer = 'X';
+        }
+
+        // Plays the audio for square selected 
+        audio('./media/place.mp3');
+        if (activePlayer === 'O') {
+            disableClick();
+            setTimeout(function () { computersTurn(); }, 1000);
+        }
+        return true;
+    }
+
+    // Nested function that takes the computers turn 
+    function computersTurn() {
+        let success = false;
+        let pickASquare;
+        while (!success) {
+            pickASquare = String(Math.floor(Math.random() * 9));
+            if (placeXOrO(pickASquare)) {
+                placeXOrO(pickASquare);
+                success = true;
+            }
+        }
+    }
+}
