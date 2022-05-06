dbd56635378954af635ff46d976830be5794fd74
sunelement <102632921+sunelement@users.noreply.github.com>
2022-05-05 18:10:48 -0700

    boot

---
+++ bootstrap4_project/main.js
@@ -0,0 +1,9 @@
+// Tooltip used by top navbar
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Displays popover explanation of badges 
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
